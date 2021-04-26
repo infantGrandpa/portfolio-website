9bd2e5df94d8f2892199f8abf303e3e9a3c289a7
infantGrandpa <babraham341@gmail.com>
2021-04-25 20:20:26 -0400

    Added basic JS functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("This is my portfolio website.");
+function menuToggle() {
+    var x = document.getElementById('my-nav-toggle');
+    if (x.className === 'nav-toggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'nav-toggle';
+    }
+}
